b98249525e7cd58e0e09ead40e90e2667edcac3c
Sanath <sanathpun@hotmail.com>
2021-10-15 12:51:27 +1100

    added images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
-import profileImage from './img/model-s.jpg'
+// import profileImage from './img/model-s.jpg'
 
 function App() {
+
+  function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
+  
+  <img src={images['doggy.png']} />
   return (
     <div className="App">
       <h1>My React App</h1>
       <h2>Author: Sanath Punchibandage</h2>
-      <img src={profileImage} alt="profile-image" />
+      <img src={images['model-s.jpg']} alt="profile-image" />
+      <img src={images['model-3.jpg']} alt="profile-image" />
     </div>
   );
 }
