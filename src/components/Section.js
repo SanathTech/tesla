33685fcdbd315a1f07110ac974095ab8a49409cf
Sanath <sanathpun@hotmail.com>
2021-10-15 16:30:42 +1100

    added buttons and text

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
-import model_s from '../images/model-s.jpg';
+import {models, model3, downarrow} from '../images/index';
 
 function Section() {
     return (
         <Wrap>
-
+            <ItemText>
+                <Title>
+                    <h1>Model S</h1>
+                </Title>
+                <p>Description</p>
+            </ItemText>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        Left Button
+                    </LeftButton>
+                    <RightButton>
+                        Right Button
+                    </RightButton>
+                </ButtonGroup>
+                <DownArrow src={downarrow} />
+            </Buttons>
         </Wrap>
     )
 }
@@ -18,6 +34,62 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: ${props => `url("${model_s}")`};
-    //background-image: url("../images/model-s.jpg");
+    background-image: url("${models}");
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; //vertical
+    align-items: center; //horizontal
+    @media (max-width: 768px) {
+        background-image: url("${models}");;
+    }
+`
+
+const ItemText = styled.div`
+    padding: 15vh;
+    text-align: center;
+`
+
+const Title = styled.div`
+    margin-bottom: 10px;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 13px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.99;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 11px;
+`
+
+const RightButton = styled(LeftButton)`
+    background: white;
+    opacity: 0.7;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    filter: invert(100%);
+    margin-bottom: 15px;
+    height: 45px;
+    animation: animateDown infinite 2s;
+`
+
+const Buttons = styled.div`
+
 `
---
+++ src/images/index.js
@@ -0,0 +1,9 @@
+import models from './model-s.jpg';
+import model3 from './model-3.jpg';
+import downarrow from './down-arrow.svg';
+
+export {
+    models,
+    model3,
+    downarrow
+};
