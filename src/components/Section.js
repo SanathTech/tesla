aae7218d7948427fa1e162c46fd2c91de8cff651
Sanath <sanathpun@hotmail.com>
2021-10-15 15:34:38 +1100

    added images

---
+++ src/components/Section.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Section() {
+    return (
+        <Wrap>
+
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    padding-top: 15vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/img/model-s.jpg');
+`
